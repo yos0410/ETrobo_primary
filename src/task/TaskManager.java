0a0504db86c768867d592e06fba829bab15e82b2
user214 <user214@PC216.system.local>
2023-05-30 11:29:32 +0900

    o

--- src/task/TaskManager.java
+++ src/task/TaskManager.java
@@ -50,7 +50,6 @@ public class TaskManager {
     public void schedule() {// RateÇ©ÇÁDelayÇ…ïœçXÇµÇΩ
 
         futureGame = scheduler.scheduleWithFixedDelay(gameTask, 0, 10, TimeUnit.MILLISECONDS);
-
         futureLog = scheduler.scheduleWithFixedDelay(logTask, 0, 1000, TimeUnit.MILLISECONDS);
     }
     /*
